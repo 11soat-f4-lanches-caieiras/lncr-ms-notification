0457e77cf409d5850f14c0d33c2e5d9f91ebc457
titoparizotto <titoparizotto@gmail.com>
2025-12-17 20:17:17 -0300

    fix: Ajuste de Beans do banco notification

--- notification/src/main/java/br/com/tp/lncr/AppNotification.java
+++ notification/src/main/java/br/com/tp/lncr/AppNotification.java
@@ -1,7 +1,12 @@
 package br.com.tp.lncr;
 
+import br.com.tp.lncr.core.adapters.notification.NotificationControllerImpl;
+import br.com.tp.lncr.core.adapters.notification.NotificationMapper;
+import br.com.tp.lncr.core.interfaces.notification.NotificationController;
+import br.com.tp.lncr.core.interfaces.notification.NotificationDatabase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = "br.com.tp.lncr")
 public class AppNotification {
@@ -10,5 +15,14 @@ public class AppNotification {
         SpringApplication.run(AppNotification.class, args);
     }
 
+    @Bean
+    public NotificationController notificationController(NotificationDatabase notificationDatabase) {
+        return new NotificationControllerImpl(notificationDatabase);
+    }
+
+    @Bean
+    public NotificationMapper notificationMapper() {
+        return new NotificationMapper();
+    }
 }
 
